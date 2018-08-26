94b0c15fa9697930b2f7c1e221c14e572268bc98
Jackie Ha <jha2@wellesley.edu>
2018-08-26 17:00:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,19 @@
 # Write your code here.
 name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end 
 
-def batch_badge_creator(name)
-  puts
+def batch_badge_creator(attendees)
+  name.collect do |name|
+    badge_maker(name)
 end 
 
-def assign_rooms(name, room_number)
+def assign_rooms(name)
+  room = 0 
+  attendees.collect do |name|
+    room += 1 
   return "Hello, #{name}! You'll be assigned to room #{room_number}!"
 end 
 
