cf6d24a8e5034f36ccee5a6602387b16ac40d6c5
Jackie Ha <jha2@wellesley.edu>
2018-08-26 16:59:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,16 @@
 # Write your code here.
-def batch_badge_creator(name)
+name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
 end 
+
+def batch_badge_creator(name)
+  puts
+end 
+
+def assign_rooms(name, room_number)
+  return "Hello, #{name}! You'll be assigned to room #{room_number}!"
+end 
+
+def printer
+  #takes results of batch_badge_creator and then assign_rooms to the screen 
